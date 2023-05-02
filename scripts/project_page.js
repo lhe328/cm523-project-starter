4a8fa0e0dfd2a8f4fecf698abb980b47c0414636
lhe328 <lhe328@bu.edu>
2023-05-02 12:53:55 -0400

    5.2

---
+++ scripts/project_page.js
@@ -0,0 +1,17 @@
+const proposal = document.querySelector("#proposal");
+
+proposal.addEventListener("click", function() {
+  window.open('https://docs.google.com/document/d/1WLrUn-WM6QAjGAsZpcTXnQQ6pOLoHfbOl8Pe-XP-NY0/edit?usp=sharing');
+})
+
+const wireframe = document.querySelector("#wireframe");
+
+wireframe.addEventListener("click", function() {
+  window.open('https://www.figma.com/file/svk6ukb4W7pr37oQFuOZzk/CM523-Wireframe?type=design&node-id=0%3A1&t=yrklO4O0Ryvs1xAg-1');
+})
+
+const uxreport = document.querySelector("#uxreport");
+
+uxreport.addEventListener("click", function() {
+  window.open('https://docs.google.com/document/d/1XxfZPxtN8_K4e_xTdRw5FC19Mq_5gP5WIi-sZyghRp0/edit?usp=sharing');
+})
